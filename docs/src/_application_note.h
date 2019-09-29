46b68542db7cd34b48431aa528d52026086a8bb0
Tilen Majerle <tilen.majerle@gmail.com>
2019-09-29 11:39:54 +0200

    Modify getting started guide

--- docs/src/_application_note.h
+++ docs/src/_application_note.h
@@ -4,27 +4,15 @@
  *
  * \section         sect_getting_started Getting started
  *
- * Library development is fully hosted on Github and there is no future plans to move to any other platform.
+ * Repository <a href="https://github.com/MaJerle/ringbuff"><b>ringbuff is hosted on Github</b></a>. It combines source code and example projects.
  *
- * There are `2` repositories
- *
- *  - <a href="https://github.com/MaJerle/ringbuff"><b>ringbuff</b></a>: Source code of library itself.
- *      - Repository is required when developing final project
- *
- *  - <a href="https://github.com/MaJerle/ringbuff_res"><b>ringbuff_res</b></a>: Resources, development code,
- *      documentation sources, examples, code snippets, etc.
- *      - This repository uses `ringbuff` repository as `submodule`
- *      - Repository is used to evaluate library using prepared examples
- *
- * \subsection      sect_clone_res Clone resources with examples
- *
- * Easiest way to test the library is to clone resources repository.
+ * \subsection      sect_clone Clone repository
  *
  * \par             First-time clone
  *
  *  - Download and install `git` if not already
  *  - Open console and navigate to path in the system to clone repository to. Use command `cd your_path`
- *  - Run `git clone --recurse-submodules https://github.com/MaJerle/ringbuff_res` command to clone repository including submodules
+ *  - Run `git clone --recurse-submodules https://github.com/MaJerle/ringbuff` command to clone repository including submodules
  *  - Navigate to `examples` directory and run favourite example
  *
  * \par             Already cloned, update to latest version
@@ -33,23 +21,8 @@
  *  - Run `git pull origin master --recurse-submodules` command to pull latest changes and to fetch latest changes from submodules
  *  - Run `git submodule foreach git pull origin master` to update & merge all submodules
  *
- * \subsection      sect_clone_lib Clone library only
- *
- * If you are already familiar with library and you wish to include it in existing project, easiest way to do so is to clone library repository only.
- 
- * \par             First-time clone
- *
- *  - Download and install `git` if not already
- *  - Open console and navigate to path in the system to clone repository to. Use command `cd your_path`
- *  - Run `git clone --recurse-submodules https://github.com/MaJerle/ringbuff` command to clone repository
- *
- * \par             Already cloned, update to latest version
- *
- *  - Open console and navigate to path in the system where your repository is. Use command `cd your_path`
- *  - Run `git pull origin master --recurse-submodules` to update & merge latest repository changes
- *
  * \section         sect_how_it_works How ring buffer works
-
+ *
  * Reference first:
  *  - `R` represents `R`ead pointer. Read on read/write operations. Modified on read operation only
  *  - `W` represents `W`rite pointer. Read on read/write operations. Modified on write operation only
