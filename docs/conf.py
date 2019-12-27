5e2aa19e3114885fd982171ec3c23e62c5fb226c
Tilen Majerle <tilen.majerle@gmail.com>
2019-12-27 21:25:20 +0100

    Update copyright name

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ subprocess.call('doxygen doxy_ringbuff.doxy', shell=True)
 # -- Project information -----------------------------------------------------
 
 project = 'Ringbuffer'
-copyright = '2019, Tilen Majerle'
+copyright = '2019, Tilen MAJERLE'
 author = 'Tilen MAJERLE'
 
 # The full version, including alpha/beta/rc tags
