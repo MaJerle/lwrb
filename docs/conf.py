cd07cb7201c1524f951c14968c23b65f64057c74
Tilen Majerle <tilen.majerle@gmail.com>
2020-04-11 15:43:59 +0200

    Set version to 1.3

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = '2020, Tilen MAJERLE'
 author = 'Tilen MAJERLE'
 
 # The full version, including alpha/beta/rc tags
-version = '1.2.0'
+version = '1.3.0'
 
 # Try to get branch at which this is running
 # and try to determine which version to display in sphinx
@@ -38,7 +38,7 @@ for line in res.split("\n"):
         git_branch = line[1:].strip()
 
 # Decision for display version
-try: 
+try:
     if git_branch.index('develop') >= 0:
         version = "latest-develop"
 except Exception:
@@ -89,7 +89,7 @@ html_theme_options = {
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
-    
+
     'logo_only': False,
 
     # Toc options
