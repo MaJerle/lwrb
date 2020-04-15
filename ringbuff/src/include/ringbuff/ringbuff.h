6ecf96e120e766fc8bf32b9784b16d3c20be783d
Tilen Majerle <tilen.majerle@gmail.com>
2020-04-15 13:08:18 +0200

    Fix missing volatile for callback function

--- ringbuff/src/include/ringbuff/ringbuff.h
+++ ringbuff/src/include/ringbuff/ringbuff.h
@@ -51,7 +51,7 @@ extern "C" {
  * \brief           Enable buffer structure pointer parameter as volatile
  * To use this feature, uncomment keyword below
  */
-#define RINGBUFF_VOLATILE               /* volatile */
+#define RINGBUFF_VOLATILE                       /* volatile */
 
 /**
  * \brief           Event type for buffer operations
@@ -73,7 +73,7 @@ struct ringbuff;
  * \param[in]       evt: Event type
  * \param[in]       bp: Number of bytes written or read (when used), depends on event type
  */
-typedef void (*ringbuff_evt_fn)(struct ringbuff* buff, ringbuff_evt_type_t evt, size_t bp);
+typedef void (*ringbuff_evt_fn)(RINGBUFF_VOLATILE struct ringbuff* buff, ringbuff_evt_type_t evt, size_t bp);
 
 /**
  * \brief           Buffer structure
