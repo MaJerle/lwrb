9193cf9b4da754c0c8ac20763ca9ff80de1f3aa4
Tofik Sonono <tofik@sonono.me>
2021-06-22 16:54:21 +0200

    Unit testing environment
    Signed-off-by: Tofik Sonono <tofik@sonono.me>

---
+++ lwrb/test/test.c
@@ -0,0 +1,37 @@
+/**
+ * @file test.c
+ *
+ * Unit tests for the lwrb library
+ *
+ * @author Tofik Sonono (tofik@sonono.me)
+ *
+ */
+
+/*======= Includes ==========================================================*/
+
+#include "unity.h"
+#include "lwrb/lwrb.h"
+
+/*======= Local Macro Definitions ===========================================*/
+/*======= Local function prototypes =========================================*/
+/*======= Local variable declarations =======================================*/
+/*======= Global function implementations ===================================*/
+
+void setUp(void) { }
+
+void tearDown(void) { }
+
+/*======= Tests ==============================================================*/
+
+void firstTest(void)
+{
+
+}
+
+/*======= Main ===============================================================*/
+
+int main (void) {
+    UNITY_BEGIN();
+    RUN_TEST(firstTest);
+    return UNITY_END();
+}
