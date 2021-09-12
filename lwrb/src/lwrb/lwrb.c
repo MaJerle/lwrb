c21ba2dce937989d3dffe23f631dc56f8b1c397b
Tilen Majerle <tilen.majerle@gmail.com>
2021-09-12 21:33:16 +0200

    Add lwrb v 2.0.2

--- lwrb/src/lwrb/lwrb.c
+++ lwrb/src/lwrb/lwrb.c
@@ -29,7 +29,7 @@
  * This file is part of LwRB - Lightweight ring buffer library.
  *
  * Author:          Tilen MAJERLE <tilen@majerle.eu>
- * Version:         v2.0.1
+ * Version:         v2.0.2
  */
 #include "lwrb/lwrb.h"
 
