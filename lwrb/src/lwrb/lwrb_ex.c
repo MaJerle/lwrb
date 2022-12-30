26fb3aa01bca935f67b5107e0ba48c4974dbbd47
jnz86 <jumpifnotzero@gmail.com>
2022-12-30 16:18:36 -0700

    Initial commit
    Adds the overwrite(). Basic tests run, no promises on edge cases.
    
    The two main functional aspects are a "linear region" (from w to end of buffer), and a wrap region (bytes remaining that loop over to the start of buffer). Determine linear, write it, determine wrap, write it, each time skipping r ahead if required.

---
+++ lwrb/src/lwrb/lwrb_ex.c
@@ -0,0 +1,106 @@
+/**
+ * \file            lwrb_ex.c
+ * \brief           Lightweight ring buffer extended functions
+ */
+
+
+#include "lwrb.h"
+
+
+#define BUF_IS_VALID(b) ((b) != NULL && (b)->buff != NULL && (b)->size > 0)
+#define BUF_MIN(x, y)   ((x) < (y) ? (x) : (y))
+
+
+/**
+ * \brief           Similar to write(), writes data to buffer, will overwrite existing values.
+ * \param[in]       buff: Buffer handle
+ * \param[in]       data: Data to write to ring buffer
+ * \param[in]       btw: Bytes To Write, length
+ * \return          Number of bytes written to buffer, will always return btw
+ * \note            Functionality is primary two parts, always writes some linear region, then
+ * writes the wrap region if there is more data to write. The r indicator is advanced if w overtakes
+ * it. This operation is a read op as well as a write op. For thread-safety mutexes may be desired,
+ * see documentation.
+ */
+size_t lwrb_overwrite(lwrb_t* buff, const void* data, size_t btw)
+{
+    size_t w, r, linear, wrap;
+    size_t size   = buff->size;
+    size_t offset = 0;
+
+    if (!BUF_IS_VALID(buff) || data == NULL || btw == 0)
+    {
+        return 0;
+    }
+
+    w = atomic_load_explicit(&buff->w, memory_order_relaxed);
+    r = atomic_load_explicit(&buff->r, memory_order_relaxed);
+
+
+
+    /* Step 1: Special handling if asked to write more bytes than size. */
+    if (btw >= size)
+    {
+        offset = (btw - size) + 1;
+        btw    = size - 1;
+    }
+
+
+
+    /* Step 2: Always write the linear region. There will be at least one byte written here. */
+
+    /* Handle special case of linear being off by one when zero */   // TODO: Test this
+    if (w == 0)
+        linear = size - 1;
+    else
+        linear = size - w;
+
+    /* We might not need the whole linear length available, depends on amount to write*/
+    if (btw < linear)
+        linear = btw;
+
+    /* If linear write will exceed r it must be updated, this may also happen in wrap region later
+     */
+    if ((w < r) && (w + linear) > r)
+    {
+        r = w + btw + 1;
+        r %= size;
+    }
+
+    /* Copy the linear region data, and prepare for the wrap region */
+    memcpy(buff->buff + w, data + offset, linear);
+    w += linear;
+    w %= size;
+    wrap = btw - linear;
+
+
+
+    /* Step 3: Wrap region are the bytes that rolled back to the start of the storage array */
+    if (wrap)
+    {
+
+        /* If r will be overrun by new write data, skip r ahead */
+        if ((w <= r) && (w + wrap) >= r)
+        {
+            r = w + wrap + 1;
+            // r %= size;   // TODO: Can this happen now?
+        }
+
+        /* Copy the wrap region data */
+        memcpy(buff->buff, data + offset + linear, wrap);
+        w += wrap;
+        w %= size;
+    }
+
+
+
+    /* Step 4: The ring buffer is complete, this is a write op as well as possibly a read op */
+    atomic_store_explicit(&buff->w, w, memory_order_release);
+    atomic_store_explicit(&buff->r, r, memory_order_release);
+
+    // TODO: It might be desirable to store r_old and only update if r has changed
+
+    // TODO: Missing event handler
+
+    return (btw);
+}
